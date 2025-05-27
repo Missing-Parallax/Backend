c416f14658fe69c841e260d4aea58181d14c8398
Missing-Parallax <104851278+Missing-Parallax@users.noreply.github.com>
2025-05-28 01:36:38 +0530

    add user route and controller and fixed asyncHandler bug

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
-    path:"./env"
-})
+  path: "./env",
+});
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running at : ${process.env.PORT}`)
-    })
-})
-.catch((error)=>{
-    console.log("Mongo DB connection failed !!! ",error);
-    
-})
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo DB connection failed !!! ", error);
+  });
